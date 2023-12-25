31ccded168fa2e295416a50576bcdf5854fb9e2a
Rikku <rikkubook@gmail.com>
2023-12-25 17:49:23 +0800

    🦄 refactor(students): posts、users 的Controllers

---
+++ controllers/postControllers.js
@@ -0,0 +1,98 @@
+const Post = require('../models/postModel')
+const Like = require('../models/likesModel')
+const User = require('../models/userModel')
+const { successHandler, errorHandler } = require('../handler');
+
+const postControl = {
+  getAllPosts:
+    async (req, res) => {
+      // http://localhost:3005/posts?timeSort=asc&search=
+      const timeSort = req.query.timeSort == 'asc'? 1:-1
+      const search = req.query.search? {"content": new RegExp(req.query.search)} : {}; 
+      try{
+        const posts =await Post.find(search).populate({ //此是先將name編譯,可以不編譯只算長度
+          path: 'userInfo',
+          select: 'name photo'
+        }).sort({'createAt': timeSort})  //.sort(timeSort)
+        successHandler(res, posts)
+      }catch(error){
+        errorHandler(res,error,400)
+      }
+    },
+  postNewPost:
+    async (req, res) => {
+      try{
+        const data = req.body
+        if(!data.userID){
+          throw '使用者ID不為空'
+        }else if(!data.content){
+          throw '內文不為空'
+        }
+    
+        if(data.userID){ // 防止沒有使用者依然可以輸入
+          const user =await User.findById(data.userID);
+          if(!user){
+            throw '沒有這位使用者'
+          }
+        }
+    
+        const newPost = await Post.create({
+            userInfo: data.userID,
+            image: data.image,
+            content: data.content,
+            likes: data.likes,
+            comments: data.comments,
+            createdAt: data.createdAt,
+          })
+        successHandler(res, newPost)
+      }catch(error){
+        errorHandler(res,error,400)
+      }
+    },
+  patchPost:
+    async (req, res) => {
+      try{
+        const id = req.params.id;
+        const data = req.body
+    
+        if(!data.content){
+          throw '內文不為空'
+        }
+    
+        const resultPost = await Post.findByIdAndUpdate(id,data);
+        if(resultPost == null){
+          throw '查無此id'
+        } // 會是找到那筆，但未修改
+    
+        const newData =await Post.findById(id);
+        successHandler(res, newData)
+      }catch(error){
+        errorHandler(res,error,400)
+      }
+    },
+  deleteAllPost:
+    async (req, res) => {
+      try{
+        await Post.deleteMany({});
+        successHandler(res, [])
+      }catch(error){
+        errorHandler(res,error,400)
+      }
+    },
+  deletePost:
+    async (req, res) => {
+      try{
+        const id = req.params.id;
+        const resultUser = await Post.findByIdAndDelete(id);
+        if(resultUser == null){
+          throw '查無此id'
+        }
+        const posts =await Post.find({});
+        successHandler(res, posts)
+      }catch(error){
+        errorHandler(res,error,400)
+      }
+    }
+}
+
+module.exports = postControl;
--- routes/users.js
+++ routes/users.js
@@ -1,87 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel')
-const { successHandler, errorHandler } = require('../handler');
+const userControl = require('../controllers/userControllers');
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
-  try{
-    const users =await User.find({});
-    successHandler(res, users)
-  }catch(error){
-    errorHandler(res,error,400)
-  }
-});
+router.get('/', userControl.getAllUsers); //只是轉打的配置
 
-router.post('/', async (req, res) => {
-  try{
-    const data =req.body
+router.post('/', userControl.postUser);
 
-    if(!data.name){
-      throw '使用者名稱不為空'
-    }else if(!data.email){
-      throw 'Email不為空'
-    }else if(!data.password){
-      throw '密碼不為空'
-    }
+router.patch('/:id', userControl.patchUser);
 
-    const newUser =await User.create({
-      name: data.name,
-      email: data.email,
-      photo: data.photo,
-      password: data.password
-    });
-    successHandler(res, newUser)
-  }catch(error){
-    errorHandler(res,error,400)
-  }
-});
+router.delete('/', userControl.deleteAllUsers);
 
-router.patch('/:id', async (req, res) => {
-  try{
-    const id = req.params.id;
-    const data = req.body
-    const newArray = Object.keys(data)
-
-    if(newArray.includes('name') && !data.name){
-      throw '名字不為空'
-    }else if(newArray.includes('email') && !data.email){
-      throw 'Email不為空'
-    }
-
-    const resultUser = await User.findByIdAndUpdate(id,data);
-    if(resultUser == null){
-      throw '查無此id'
-    }
-    const newData =await User.findById(id);
-    console.log(newData)
-    successHandler(res, newData)
-  }catch(error){
-    errorHandler(res,error,400)
-  }
-});
-
-router.delete('/', async (req, res) => {
-  try{
-    await User.deleteMany({});
-    successHandler(res, [])
-  }catch(error){
-    errorHandler(res,error,400)
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try{
-    const id = req.params.id;
-    const resultUser = await User.findByIdAndDelete(id);
-    if(resultUser == null){
-      throw '查無此id'
-    }
-    const users =await User.find({});
-    successHandler(res, users)
-  }catch(error){
-    errorHandler(res,error,400)
-  }
-});
+router.delete('/:id', userControl.deleteUser);
 
 module.exports = router;
